30ce5febce4cdc938907b35ee87ff9f9c660795c
Graham Wood <grahamwood13@gmail.com>
2019-04-02 12:39:31 -0400

    Imp: Requests for pages
    Ref #4

--- wikipedia.py
+++ wikipedia.py
@@ -9,3 +9,11 @@ criticalEndanger = "/wiki/IUCN_Red_List_critically_endangered_species_(Animalia)
 normalEndanger = "/wiki/IUCN_Red_List_endangered_species_(Animalia)"
 vulnerable = "/wiki/IUCN_Red_List_vulnerable_species_(Animalia)"
 nearThreatened = "/wiki/IUCN_Red_List_near_threatened_species_(Animalia)"
+
+extinctWildResponse = requests.get(baseSite + extinctWild)
+criticalEndangerResponse = requests.get(baseSite + criticalEndanger)
+normalEndangerResponse = requests.get(baseSite + normalEndanger)
+vulnerableResponse = requests.get(baseSite + vulnerable)
+nearThreatenedResponse = requests.get(baseSite + nearThreatened)
+
+
