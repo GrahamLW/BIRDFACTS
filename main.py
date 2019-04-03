94206d11fb5ede8ed06c27774be9e2f0bf12b923
Graham Wood <grahamwood13@gmail.com>
2019-04-03 19:42:22 -0400

    Imp: main function, dataset tester
    Ref #4

--- main.py
+++ main.py
@@ -5,13 +5,22 @@
 #Mostly for the sleep function, which lets us
 #put pauses between tweets
 import time
-
 #Importing the tweeting functions
 import tweet
-
 #Import the wikipedia access functions
 import wikipedia
+#Import random library
+import random
 
-# def main ():
-# 	while true:
+def selector(dictionary):
+	return random.choice(list(dictionary.items()))
+
+def main ():
+	dictionary = wikipedia.updateDictionary()
+	secondsString = input("Select time in seconds between tweets ")
+	seconds = int(secondsString)
+	while True:
+		species, status = selector(dictionary)
+		tweet = species + " is currently considered to be " + status
+		timedTweet(tweets, seconds)
 
--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ import main
 import tweet
 import time
 import authenticator
+import wikipedia
 
 api = authenticator.authenticate()
 #Automated Testing File
@@ -14,18 +15,29 @@ print(user.name)
 assert user.name == "Conservation Golem"
 print("######USER CHECK COMPLETE#######")
 
+print("#########Dataset Check##########")
+dictionary = wikipedia.updateDictionary()
+x= 0
+while x < 100:
+	species, status = main.selector(dictionary)
+	assert species, status in dictionary
+	x = x + 1
+print("#####Dataset Check Complete#####")
+
+
+
 #A small scale test, should probably be done at the end of the file
 #or commented out when not wanted
-print("######TEST TWEET 24 HOURS#######")
-def testTweetsForDay():
-	base = "Automated Test Tweet "
-	i = 2
-	while i != 26:
-		tweet.tweetInput(base + str(i))
-		i = i + 1
-		time.sleep (3600)
+# print("######TEST TWEET 24 HOURS#######")
+# def testTweetsForDay():
+# 	base = "Automated Test Tweet "
+# 	i = 2
+# 	while i != 26:
+# 		tweet.tweetInput(base + str(i))
+# 		i = i + 1
+# 		time.sleep (3600)
 
-testTweetsForDay()		
-print("######TEST TWEET COMPLETE#######")
+# testTweetsForDay()		
+# print("######TEST TWEET COMPLETE#######")
 
 
