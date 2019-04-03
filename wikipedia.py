0e75a7b04eccdf96fb5851c22b344b9b8ec33372
Graham Wood <grahamwood13@gmail.com>
2019-04-03 16:44:46 -0400

    Imp: Polished Dictionary Function
    Ref #4

--- wikipedia.py
+++ wikipedia.py
@@ -3,6 +3,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+#List of phrases we do not want in our final dictionary
+exclusionList = ["edit", "Category", "List", "Conservation", "species", "subspecies", "variety",
+					"Binomial", "Trinomial", "/wiki/Fish", "/wiki/Mollusca", "/wiki/Animalia", "/wiki/Aves",
+					"/wiki/Reptilia", "/wiki/Fish", "/wiki/Mamallia", "/wiki/Amphibia", "/wiki/Arthropoda"]
+
 baseSite = "http://en.wikipedia.org"
 extinctWild = "/wiki/List_of_extinct_in_the_wild_animals"
 criticalEndanger = "/wiki/IUCN_Red_List_critically_endangered_species_(Animalia)"
@@ -11,42 +16,45 @@ vulnerable = "/wiki/IUCN_Red_List_vulnerable_species_(Animalia)"
 nearThreatened = "/wiki/IUCN_Red_List_near_threatened_species_(Animalia)"
 
 extinctWildResponse = requests.get(baseSite + extinctWild)
-# criticalEndangerResponse = requests.get(baseSite + criticalEndanger)
-# normalEndangerResponse = requests.get(baseSite + normalEndanger)
-# vulnerableResponse = requests.get(baseSite + vulnerable)
-# nearThreatenedResponse = requests.get(baseSite + nearThreatened)
+criticalEndangerResponse = requests.get(baseSite + criticalEndanger)
+normalEndangerResponse = requests.get(baseSite + normalEndanger)
+vulnerableResponse = requests.get(baseSite + vulnerable)
+nearThreatenedResponse = requests.get(baseSite + nearThreatened)
 
-bsObj = BeautifulSoup(extinctWildResponse.content, "html.parser")
+extinctWildBSOBJ = BeautifulSoup(extinctWildResponse.content, "html.parser")
+extinctWildBSOBJ = extinctWildBSOBJ.find(id = "bodyContent")
+criticalEndangerBSOBJ = BeautifulSoup(criticalEndangerResponse.content, "html.parser")
+criticalEndangerBSOBJ = criticalEndangerBSOBJ.find(id = "bodyContent")
 
-# for tag in bsObj.body.children:
-#   print(tag.name)
-#   if tag.name is not None:
-#     print(tag.attrs)
-
-bsObj = bsObj.find(id = "bodyContent")
-
-
-elemsKeys = [
-  tag.get('title')
-  for tag in bsObj.find_all('a')
-]
-
-elemsValues = [
-  tag.get('href')
-  for tag in bsObj.find_all('a')
-]
-
-#cleans out things we don't want from the 
-#keys and values are arrays of the same length
-def cleaner(keys, values):
+#takes in a Beautiful Soup Object and returns a dictionary
+def pageToDict(bsObj):
+	#make a lis tof our keys
+	keys = [
+	  tag.get('title')
+	  for tag in bsObj.find_all('a')
+	]
+	#make a list of our values
+	values = [
+	  tag.get('href')
+	  for tag in bsObj.find_all('a')
+	]
 	i = 0
-	while i < len(keys)
-		if "edit" not in elemsValues[i]
+	while i < len(keys):
+		deleted = False
+		for exclusion in exclusionList:
+			if exclusion in values[i]\
+			 or keys[i] == None:
+				print(i)
+				keys.pop(i)
+				values.pop(i)
+				deleted = True
+				break
+		if deleted == False:
 			i = i + 1
-		keys.pop(i)
-		values.pop(i)
+	return dict(zip(keys, values))
 
-def dictMaker(keys, values)
+print(pageToDict(extinctWildBSOBJ))
+
 
 
 
