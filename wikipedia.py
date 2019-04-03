a8aff199f4c8e5cda228a093b9ff6e826c4b7aa4
Graham Wood <grahamwood13@gmail.com>
2019-04-03 15:07:25 -0400

    Imp: List and Dictionary making
    Ref #4

--- wikipedia.py
+++ wikipedia.py
@@ -1,7 +1,7 @@
 #Graham Wood
 
 import requests
-import bs4 #Beautiful Soup
+from bs4 import BeautifulSoup
 
 baseSite = "http://en.wikipedia.org"
 extinctWild = "/wiki/List_of_extinct_in_the_wild_animals"
@@ -11,9 +11,42 @@ vulnerable = "/wiki/IUCN_Red_List_vulnerable_species_(Animalia)"
 nearThreatened = "/wiki/IUCN_Red_List_near_threatened_species_(Animalia)"
 
 extinctWildResponse = requests.get(baseSite + extinctWild)
-criticalEndangerResponse = requests.get(baseSite + criticalEndanger)
-normalEndangerResponse = requests.get(baseSite + normalEndanger)
-vulnerableResponse = requests.get(baseSite + vulnerable)
-nearThreatenedResponse = requests.get(baseSite + nearThreatened)
+# criticalEndangerResponse = requests.get(baseSite + criticalEndanger)
+# normalEndangerResponse = requests.get(baseSite + normalEndanger)
+# vulnerableResponse = requests.get(baseSite + vulnerable)
+# nearThreatenedResponse = requests.get(baseSite + nearThreatened)
+
+bsObj = BeautifulSoup(extinctWildResponse.content, "html.parser")
+
+# for tag in bsObj.body.children:
+#   print(tag.name)
+#   if tag.name is not None:
+#     print(tag.attrs)
+
+bsObj = bsObj.find(id = "bodyContent")
+
+
+elemsKeys = [
+  tag.get('title')
+  for tag in bsObj.find_all('a')
+]
+
+elemsValues = [
+  tag.get('href')
+  for tag in bsObj.find_all('a')
+]
+
+#cleans out things we don't want from the 
+#keys and values are arrays of the same length
+def cleaner(keys, values):
+	i = 0
+	while i < len(keys)
+		if "edit" not in elemsValues[i]
+			i = i + 1
+		keys.pop(i)
+		values.pop(i)
+
+def dictMaker(keys, values)
+
 
 
