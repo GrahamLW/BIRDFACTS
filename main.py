82789f530e0f7f37c42b2853e98af8fa5ea5c677
Graham Wood <grahamwood13@gmail.com>
2019-04-03 19:44:41 -0400

    Imp: Version of main that only runs for 20 tweets

--- main.py
+++ main.py
@@ -15,9 +15,18 @@ import random
 def selector(dictionary):
 	return random.choice(list(dictionary.items()))
 
+def shortSeries():
+	dictionary = wikipedia.updateDictionary()
+	secondsString = input("Select time in seconds between tweets, I recommend 3600 ")
+	seconds = int(secondsString)
+	for i in 20:
+		species, status = selector(dictionary)
+		tweet = species + " is currently considered to be " + status
+		timedTweet(tweets, seconds)
+
 def main ():
 	dictionary = wikipedia.updateDictionary()
-	secondsString = input("Select time in seconds between tweets ")
+	secondsString = input("Select time in seconds between tweets, I recommend 3600 ")
 	seconds = int(secondsString)
 	while True:
 		species, status = selector(dictionary)
